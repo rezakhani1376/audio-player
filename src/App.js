413d30d4a639b220ac06dbe03137d8ed673cb5b0
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2023-07-08 08:20:08 +0330

    change: handle play button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
+import classes from "./App.module.css";
 import Header from "./components/Header";
+import Play from "./components/PlayButton";
+import Pause from "./components/PauseButton";
+import { useState } from "react";
 
 function App() {
+  const [isPlay, setIsPlay] = useState(false);
+  const onClickHandler = () => {
+    setIsPlay((prevState) => !prevState);
+  };
   return (
-    <div className="App">
+    <div className={classes.App}>
       <Header />
+      <div className={classes.wrapper}>
+        {isPlay ? (
+          <Pause isPlay={isPlay} onClick={onClickHandler} />
+        ) : (
+          <Play isPlay={isPlay} onClick={onClickHandler} />
+        )}
+      </div>
+      
     </div>
   );
 }
